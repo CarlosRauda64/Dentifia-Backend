6f400873044e4ef8abd691e95a7564bd3d0e65f3
CarlosRauda64 <rauda.carlos6432@gmail.com>
2025-06-15 18:40:20 -0600

    :sparkles: Endpoint para operaciones DELETE UPDATE y GET

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, re_path
+from rest_framework import routers
 from . import views
 from rest_framework_simplejwt.views import(
     TokenRefreshView,
 )
+
+router = routers.DefaultRouter()
+
+router.register('api_usuarios', views.UsuarioViewSet, basename='usuarios')
 
 urlpatterns = [
     path('login', views.login),
@@ -12,3 +17,5 @@ urlpatterns = [
     path('token', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += router.urls
--- usuarios/views.py
+++ usuarios/views.py
@@ -7,7 +7,7 @@ from .serializers import (
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import Usuario
-from rest_framework import status
+from rest_framework import status, permissions, viewsets
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
@@ -20,17 +20,6 @@ def login(request):
 
     return Response({"usuario": serializer.data},
                     status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def crear(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        user = Usuario.objects.get(usuario=serializer.data['usuario'])
-        user.set_password(serializer.data['password'])
-        user.save()
-        return Response({"usuario": serializer.data.get("usuario")}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -49,6 +38,23 @@ def profile(request):
     return Response(filtered_data, 
                     status=status.HTTP_200_OK)
 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer 
+
+""" Crear un nuevo usuario. """
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def crear(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = Usuario.objects.get(usuario=serializer.data['usuario'])
+        user.set_password(serializer.data['password'])
+        user.save()
+        return Response({"usuario": serializer.data.get("usuario")}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+""" Listar usuarios registrados. """
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def listar_usuarios(request):
@@ -67,5 +73,8 @@ def listar_usuarios(request):
         })
     return Response(filtered_data, status=status.HTTP_200_OK)
 
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer 
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['get', 'put', 'patch', 'delete']
