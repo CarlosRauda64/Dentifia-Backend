7b3fa8827048755f9a9cc509b5999d0d5c41c336
goldenMona <147994876+goldenMona@users.noreply.github.com>
2025-06-21 21:27:15 -0600

    Modelo de inventario casi terminado

--- inventario/models.py
+++ inventario/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from usuarios.models import Usuario
+
 # Create your models here.
 class Insumo(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=256)
-    stock_actual = models.IntegerField()
+    stock_actual = models.IntegerField(default=0)
 
 class Movimiento_stock(models.Model):
     tipo = models.CharField(max_length=10)
     fecha = models.DateField(auto_now_add=True)
     cantidad = models.IntegerField()
     insumo = models.ForeignKey(Insumo, on_delete=models.CASCADE, related_name='movimientos')
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='movimientos_stock_realizados')
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True, related_name='movimientos_stock_realizados')
+    """ Campos manejados con Snapshot """
+    nombre_usuario = models.CharField(max_length=150)
+    rol_usuario = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
