69867a836cc6316512e79883be68401f09d4bd6a
CarlosRauda64 <rauda.carlos6432@gmail.com>
2025-06-23 11:03:15 -0600

    Deploy en Render

--- dentifia/settings.py
+++ dentifia/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from corsheaders.defaults import default_methods 
 from corsheaders.defaults import default_headers
 from datetime import timedelta
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*5h3)2!&cd#_3l^r8j!44lk7()$0%^tzd#66e)4jxw9_60c_(('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*5h3)2!&cd#_3l^r8j!44lk7()$0%^tzd#66e)4jxw9_60c_((')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,14 +85,20 @@ WSGI_APPLICATION = 'dentifia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default= os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -127,6 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+    # Archivos estáticos en producción
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
