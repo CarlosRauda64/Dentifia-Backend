2347eb316e7ef0ab0eb5f3b7807f5a165a5685cd
KikeHerrera <149346367+KikeHerrera@users.noreply.github.com>
2025-06-24 01:38:08 -0600

    Ya sirven los 3

--- facturacion/urls.py
+++ facturacion/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import views
 from .views import FacturaViewSet
 from .views import FacturaViewSet, DetalleFacturaViewSet
 
@@ -11,4 +12,5 @@ router.register(r'detalles', DetalleFacturaViewSet, basename='detallefactura')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('eliminar/<str:pk>/', views.eliminar_factura, name='eliminar_factura'),
 ]
