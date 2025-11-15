f82503382e2e71c7b4cf15d03b9eaebda1134150
Carlos Rauda <rauda.carlos6432@gmail.com>
2025-11-15 06:28:39 -0600

    Add corsheaders

--- dentifia/settings.py
+++ dentifia/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
