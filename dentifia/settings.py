da5b114611b8ae312ef64d409ad8eef15e082190
Carlos Rauda <rauda.carlos6432@gmail.com>
2025-11-15 06:30:31 -0600

    Add corsheaders

--- dentifia/settings.py
+++ dentifia/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['https://frontend-dentifia.vercel.app/']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
