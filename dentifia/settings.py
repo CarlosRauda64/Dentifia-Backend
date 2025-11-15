cdea35b7f70cb906c7f3f9c546d0eb291a6f303c
Carlos Rauda <rauda.carlos6432@gmail.com>
2025-11-15 04:40:09 -0600

    deploy render

--- dentifia/settings.py
+++ dentifia/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5h3)2!&cd#_3l^r8j!44lk7()$0%^tzd#66e)4jxw9_60c_(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
