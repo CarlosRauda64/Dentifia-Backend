370d43cc206f4abee000cba3171303d42ecf62a3
Carlos Rauda <rauda.carlos6432@gmail.com>
2025-11-16 07:04:10 -0600

    Fix media root

--- dentifia/urls.py
+++ dentifia/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
     path('citas/', include('citas.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
